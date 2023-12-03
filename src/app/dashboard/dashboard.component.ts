d6d2daac63c646df55cf589584183ff8cba08c91
Ritesh Prasad (ritepras) <ritepras@cisco.com>
2023-12-03 19:21:34 +0530

    first commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+})
+export class DashboardComponent implements OnInit
+{
+  Designation: string = "";
+  Username: string = "";
+  NoOfTeamMembers: number = 0;
+  TotalCostOfAllProjects: number = 0;
+  PendingTasks: number = 0;
+  UpComingProjects: number = 0;
+  ProjectCost: number = 0;
+  CurrentExpenditure: number = 0;
+  AvailableFunds: number = 0;
+
+  Clients: string[] = [];
+  Projects: string[] = [];
+  Years: number[] = [];
+  TeamMembersSummary: any = [];
+  TeamMembers: any = [];
+
+  ngOnInit()
+  {
+    this.Designation = 'Team Leader';
+    this.Username = 'Srinivasa Neppalli';
+    this.NoOfTeamMembers = 10;
+    this.TotalCostOfAllProjects = 20;
+    this.PendingTasks = 12;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 2113507;
+    this.CurrentExpenditure = 96788;
+    this.AvailableFunds = 52536;
+  }
+}
+
